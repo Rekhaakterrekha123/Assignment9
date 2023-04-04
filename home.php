b0a381a131be5cf01909955853a266bf31a87176
Rekhaakterrekha123 <125435510+Rekhaakterrekha123@users.noreply.github.com>
2023-04-04 22:04:06 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage - My Website</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="logo">
+			<img src="logo.png" alt="My Website">
+		</div>
+		<nav>
+			<ul>
+				<li><a href="home.php">Home</a></li>
+			
+				<li><a href="Blog.php">Blog</a></li>
+				<li><a href="Contact.php">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+	<section class="featured-post">
+		<h2>Latest Blog Post</h2>
+		<div class="post">
+			<img src="featured-post-image.jpg" alt="Featured Post Image">
+			<h3><a href="#">Title of the Latest Blog Post</a></h3>
+			<p>Excerpt from the latest blog post goes here.</p>
+		</div>
+	</section>
+	<footer>
+		<p>&copy; My Website 2023. All Rights Reserved.</p>
+	</footer>
+</body>
+</html>
+
